e0493ad871a5936539fd0aaf6c581556ee012c7d
refractionpcsx2 <refraction@gmail.com>
2017-01-15 14:51:45 +0000

    Missed the cpu header

--- RefNES/cpu.h
+++ RefNES/cpu.h
@@ -8,4 +8,13 @@ extern unsigned short PC;
 extern unsigned char Opcode;
 extern unsigned char X;
 extern unsigned char Y;
+
+#define CARRY_FLAG (1 << 0)
+#define ZERO_FLAG (1 << 1)
+#define INTERRUPT_DISABLE_FLAG (1 << 2)
+#define DECIMAL_FLAG (1 << 3)
+#define BREAK_FLAG (1 << 4)
+#define OVERFLOW_FLAG (1 << 6)
+#define NEGATIVE_FLAG (1 << 7)
+
 #endif
