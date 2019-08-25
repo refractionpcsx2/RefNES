2def854da76af4da2576b13aba3f92449a5f93f6
refractionpcsx2 <refraction@gmail.com>
2019-08-25 19:32:42 +0100

    Dramatically sped up the rendering

--- RefNES/SDLInterface.cpp
+++ RefNES/SDLInterface.cpp
@@ -11,7 +11,8 @@ extern HWND hwndSDL;
 extern int prev_v_cycle;
 extern char MenuVSync;
 unsigned int ScreenBuffer[256][240];
-
+Uint32* pixels = nullptr;
+int pitch = 0;
 
 void DestroyDisplay() {
 	if (SDL_MUSTLOCK(SDL_Display)) {
@@ -59,6 +60,9 @@ void InitDisplay(int width, int height, HWND hWnd)
 	prev_v_cycle = SDL_GetTicks();
 	SetFocus(hwndSDL);
 
+
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 256, 240);
+
 }
 unsigned int xpos = 0;
 
@@ -72,38 +76,41 @@ void DrawPixelBuffer(int ypos, int xpos, unsigned int pixel)
 }
 
 void DrawScreen() {
-	int scale = SCREEN_WIDTH / 256;
-
-	SDL_Rect rect;
 
 	for (int xpos = 0; xpos < 256; xpos++) {
 		for (int ypos = 1; ypos < 240; ypos++) {
-			rect = { xpos*scale,ypos*scale,scale,scale };
-			SDL_FillRect(SDL_Display, &rect, ScreenBuffer[xpos][ypos]);
+            if (ScreenBuffer[xpos][ypos])
+            {
+                //CPU_LOG("Start Scene draw pixel\n");
+                unsigned int position = ypos * (pitch / sizeof(unsigned int)) + xpos;
+                pixels[position] = (ScreenBuffer[xpos][ypos] & 0xFF) << 8 | ((ScreenBuffer[xpos][ypos] >> 8) & 0xFF) << 16 | ((ScreenBuffer[xpos][ypos] >> 16) & 0xFF) << 24 | ((ScreenBuffer[xpos][ypos] >> 24) & 0xFF);
+            }
 		}
 	}
 }
 
 void EndDrawing()
 {
+    int scale = SCREEN_WIDTH / 256;
+    SDL_Rect srcrect;
+    SDL_Rect destrect;
 
-	texture = SDL_CreateTextureFromSurface(renderer, SDL_Display);
-	
-	if (SDL_MUSTLOCK(SDL_Display)) {
-		SDL_UnlockSurface(SDL_Display);
-	}
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-
-	SDL_RenderPresent(renderer);
-	SDL_DestroyTexture(texture);
+    srcrect = { 0,0,256,240 };
+    destrect = { 0,0,256*scale,240*scale };
+    SDL_UnlockTexture(texture);
+	SDL_RenderCopy(renderer, texture, &srcrect, &destrect);
+    SDL_RenderPresent(renderer);
 }
 
 void StartDrawing()
 {
-	//CPU_LOG("Start Scene");
-	if (SDL_MUSTLOCK(SDL_Display))
-	{
-		SDL_LockSurface(SDL_Display);
-	}
+    int scale = SCREEN_WIDTH / 256;
+    CPU_LOG("Start Scene");
+    SDL_RenderClear(renderer);
+
+    if (SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch))
+    {
+       abort();
+    }
+    
 }
